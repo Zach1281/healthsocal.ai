8bed24d0779089df1955f2eb3a4501060f0bf0fe
Zach1281 <zachary_romero@yahoo.com>
2023-01-29 20:53:40 -0800

    updated with the paraguay tutorial

--- tutorial projects/choro_tut/src/App.vue
+++ tutorial projects/choro_tut/src/App.vue
@@ -1,28 +1,62 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <l-map :center="[-23.752961, -57.854357]" :zoom="6" style="height: 500px;" :options="mapOptions">
+      <l-choropleth-layer :data="pyDepartmentsData" titleKey="department_name" idKey="department_id" :value="value" :extraValues="extraValues" geojsonIdKey="dpto" :geojson="paraguayGeojson" :colorScale="colorScale">
+        <template slot-scope="props">
+          <l-info-control :item="props.currentItem" :unit="props.unit" title="Department" placeholder="Hover over a department"/>
+          <l-reference-chart title="Girls school enrolment" :colorScale="colorScale" :min="props.min" :max="props.max" position="topright"/>
+        </template>
+      </l-choropleth-layer>
+    </l-map>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { InfoControl, ReferenceChart, ChoroplethLayer } from 'vue-choropleth'
+
+//import { geojson } from './data/py-departments-geojson'
+import paraguayGeojson from './data/paraguay.json'
+import { pyDepartmentsData } from './data/py-departments-data'
+import {LMap} from 'vue2-leaflet';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "app",
+  components: { 
+    LMap,
+    'l-info-control': InfoControl, 
+    'l-reference-chart': ReferenceChart, 
+    'l-choropleth-layer': ChoroplethLayer 
+  },
+  data() {
+    return {
+      pyDepartmentsData,
+      paraguayGeojson,
+      colorScale: ["e7d090", "e9ae7b", "de7062"],
+      value: {
+        key: "amount_w",
+        metric: "% girls"
+      },
+      extraValues: [{
+        key: "amount_m",
+        metric: "% boys"
+      }],
+      mapOptions: {
+        attributionControl: false
+      },
+      currentStrokeColor: '3d3213'
+    }
+  },
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import "../node_modules/leaflet/dist/leaflet.css";
+body {
+  background-color: #e7d090;
+  margin-left: 100px;
+  margin-right: 100px;
+}
+
+#map {
+  background-color: #eee;
 }
 </style>
