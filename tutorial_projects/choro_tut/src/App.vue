28b4cf777ab7981cc80c88380af9658fd8ad4241
Erick Gutierrez <78944178+erickdg2000@users.noreply.github.com>
2023-01-31 20:23:20 -0800

    added OpenStreetMap layer to map and copywrite attribute

--- tutorial_projects/choro_tut/src/App.vue
+++ tutorial_projects/choro_tut/src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="app">
     <l-map :center="[-23.752961, -57.854357]" :zoom="6" style="height: 500px;" :options="mapOptions">
+      <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
       <l-choropleth-layer :data="pyDepartmentsData" titleKey="department_name" idKey="department_id" :value="value" :extraValues="extraValues" geojsonIdKey="dpto" :geojson="paraguayGeojson" :colorScale="colorScale">
         <template slot-scope="props">
           <l-info-control :item="props.currentItem" :unit="props.unit" title="Department" placeholder="Hover over a department"/>
-          <l-reference-chart title="Girls school enrolment" :colorScale="colorScale" :min="props.min" :max="props.max" position="topright"/>
+          <l-reference-chart title="Girls school enrollment" :colorScale="colorScale" :min="props.min" :max="props.max" position="topright"/>
         </template>
       </l-choropleth-layer>
     </l-map>
@@ -17,18 +18,21 @@ import { InfoControl, ReferenceChart, ChoroplethLayer } from 'vue-choropleth'
 //import { geojson } from './data/py-departments-geojson'
 import paraguayGeojson from './data/paraguay.json'
 import { pyDepartmentsData } from './data/py-departments-data'
-import {LMap} from 'vue2-leaflet';
+import {LMap, LTileLayer} from 'vue2-leaflet';
 
 export default {
   name: "app",
   components: { 
     LMap,
+    LTileLayer,
     'l-info-control': InfoControl, 
     'l-reference-chart': ReferenceChart, 
     'l-choropleth-layer': ChoroplethLayer 
   },
   data() {
     return {
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      attribution: '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
       pyDepartmentsData,
       paraguayGeojson,
       colorScale: ["e7d090", "e9ae7b", "de7062"],
@@ -40,9 +44,9 @@ export default {
         key: "amount_m",
         metric: "% boys"
       }],
-      mapOptions: {
+      /*mapOptions: {
         attributionControl: false
-      },
+      },*/
       currentStrokeColor: '3d3213'
     }
   },
