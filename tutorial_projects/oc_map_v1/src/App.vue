8dbe41226d52454ecbf7328b383d01322d73f30d
Zach1281 <zachary_romero@yahoo.com>
2023-02-01 23:03:58 -0800

    updated new map with oc data

---
+++ tutorial_projects/oc_map_v1/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <l-map :center="[33.7175, -117.8311]" :zoom="10" style="height: 500px;" :options="mapOptions">
+      <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+      <l-choropleth-layer :data="ocMHData" titleKey="zip" idKey="zip_code" :value="value" geojsonIdKey="dtpo" :geojson="ocGeojson" :colorScale="colorScale">
+        <template slot-scope="props">
+          <l-info-control :item="props.currentItem" :unit="props.unit" title="Zip Code" placeholder="Hover over a zip code"/>
+          <l-reference-chart title="Mental Health Index Averages by Zip Code" :colorScale="colorScale" :min="props.min" :max="props.max" position="topright"/>
+        </template>
+      </l-choropleth-layer>
+    </l-map>
+  </div>
+</template>
+
+<script>
+import { InfoControl, ReferenceChart, ChoroplethLayer } from 'vue-choropleth'
+
+//import { geojson } from './data/py-departments-geojson'
+import ocGeojson from './data/ocGeoZipCode.json'
+import { ocMHData } from './data/averaged_data_zipcode'
+import {LMap, LTileLayer} from 'vue2-leaflet';
+
+export default {
+  name: "app",
+  components: { 
+    LMap,
+    LTileLayer,
+    'l-info-control': InfoControl, 
+    'l-reference-chart': ReferenceChart, 
+    'l-choropleth-layer': ChoroplethLayer 
+  },
+  data() {
+    return {
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      attribution: '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      ocMHData,
+      ocGeojson,
+      colorScale: ["e7d090", "e9ae7b", "de7062"],
+      value: {
+        key: "mhi_avg",
+        metric: "Mental Health Index Average"
+      },
+      mapOptions: {
+        attributionControl: false
+      },
+      currentStrokeColor: '3d3213'
+    }
+  },
+}
+</script>
+<style>
+@import "../node_modules/leaflet/dist/leaflet.css";
+body {
+  background-color: #e7d090;
+  margin-left: 100px;
+  margin-right: 100px;
+}
+
+#map {
+  background-color: rgb(0, 0, 0);
+}
+</style>
