b2c02cc24cac31ff6ad79a8244a46f4ded753424
Erick Gutierrez <78944178+erickdg2000@users.noreply.github.com>
2023-02-08 14:27:05 -0800

    Co-authored-by: XuanYuL5 <XuanYuL5@users.noreply.github.com> Co-authored-by: Zachary Romero <Zach1281@users.noreply.github.com> Co-authored-by: tiffanyttran <tiffanyttran@users.noreply.github.com>

--- map_versions/oc_map_v2/src/App.vue
+++ map_versions/oc_map_v2/src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <input type="radio" id="map1" value="map1" v-model="showMap">
-    <label for="map1">Map 1</label>
-    <input type="radio" id="map2" value="map2" v-model="showMap">
-    <label for="map2">Map 2</label>
-    <div v-if="showMap === 'map1'" id="app">
+    <input type="radio" id="map1" value="map1" v-model="selectedMap" @change="updateMap">
+    <label for="map1">MHI</label>
+    <input type="radio" id="map2" value="map2" v-model="selectedMap" @change="updateMap">
+    <label for="map2">SDOH</label>
+    <div v-if="selectedMap === 'map1'" id="app">
       <l-map :center="[33.7175, -117.8311]" :zoom="10" style="height: 750px;" :options="mapOptions">
         <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
         <l-choropleth-layer :data="ocMHData" titleKey="zip" idKey="zip_code" :value="value" :extraValues="extraValues" geojsonIdKey="dpto" :geojson="ocGeojson" :colorScale="colorScale">
@@ -16,13 +16,13 @@
       </l-map>
     </div>
   <div>
-    <div v-if="showMap === 'map2'">
+    <div v-if="selectedMap === 'map2'">
       <l-map :center="[33.7175, -117.8311]" :zoom="10" style="height: 750px;" :options="mapOptions">
         <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
         <l-choropleth-layer :data="sdohData" titleKey="ZIPCODE" idKey="ZCTA" :value="value" geojsonIdKey="dpto" :geojson="ocGeojson" :colorScale="colorScale">
           <template slot-scope="info">
             <l-info-control :item="info.currentItem" :unit="info.unit" title="Zip Code" placeholder="Hover over a zip code"/>
-            <l-reference-chart title="Mental Health Index Averages by Zip Code" :colorScale="colorScale" :min="info.min" :max="info.max" position="topright"/>
+            <l-reference-chart title="Social Determinants of Health by Zip Code" :colorScale="colorScale" :min="info.min" :max="info.max" position="topright"/>
           </template>
         </l-choropleth-layer>
       </l-map>
@@ -50,34 +50,28 @@ export default {
     'l-choropleth-layer': ChoroplethLayer 
   },
   data() {
-    return {
-      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-      attribution: '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      ocMHData,
-      ocGeojson,
-      sdohData,
-      showMap: 'map2',
-      colorScale: ["71ae46", "ebe12a", "ac2026"],
-      value: {
-        key: "ACS_PCT_HH_NO_FD_STMP_BLW_POV_ZC",
-        metric: "Food Stamps"
-      },
-      value1:{
-        key: "mhi_value",
-        metric: "Mental Health Index"
-      },
-      mapOptions: {
-        attributionControl: false
-      },
-      currentStrokeColor: '3d3213'
-    }
+      return {
+        url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        attribution: '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+        ocMHData,
+        ocGeojson,
+        sdohData,
+        selectedMap: 'map2',
+        colorScale: ["71ae46", "ebe12a", "ac2026"],
+        value: {
+          key: "ACS_PCT_HH_NO_FD_STMP_BLW_POV_ZC",
+          metric: "Food Stamps"
+        },
+        mapOptions: {
+          attributionControl: false
+        },
+        currentStrokeColor: '3d3213'
+      }
+    
   },
   methods: {
-    toggleMap1 () {
-      this.showMap1 = !this.showMap1
-    },
-    toggleMap2 () {
-      this.showMap2 = !this.showMap2
+    updateMap(){  
+      console.log('Map updated:', this.selectedMap)
     }
   }
 }
