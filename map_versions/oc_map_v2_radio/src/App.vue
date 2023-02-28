359a81eb14d6f302a4adece1a4bb2dcedd391e3a
Zach1281 <zachary_romero@yahoo.com>
2023-02-27 21:02:15 -0800

    added weighted mhi data
    again, idk if i did the weighting properly but it's there

--- map_versions/oc_map_v2_radio/src/App.vue
+++ map_versions/oc_map_v2_radio/src/App.vue
@@ -13,7 +13,7 @@
     <div v-if="selectedMap === 'map1'" id="app">
       <l-map :center="[33.7175, -117.8311]" :zoom="10" style="height: 750px;" :options="mapOptions">
         <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-        <l-choropleth-layer :data="ocMHData" titleKey="zip" idKey="zip_code" :value="ocValue" geojsonIdKey="dpto" :geojson="ocGeojson" :colorScale="colorScale">
+        <l-choropleth-layer :data="weighted_mhi_data" titleKey="zcta" idKey="zcta" :value="ocValue" geojsonIdKey="dpto" :geojson="ocGeojson" :colorScale="colorScale">
           <template slot-scope="info">
               <l-info-control :item="info.currentItem" :unit="info.unit" title="Zip Code" placeholder="Hover over a zip code"/>
             <l-reference-chart title="Mental Health Index Averages by Zip Code" :colorScale="colorScale" :min="info.min" :max="info.max" position="topright"/>
@@ -84,6 +84,7 @@ import { sdohData } from './data/sdohData'
 import ocGeojson from './data/ocGeoZipCode.json'
 import { ocMHData } from './data/avg_data_zipcode'
 import { weightedSDOHData } from './data/weighted_sdoh_data'
+import { weighted_mhi_data } from './data/weighted_mhi_data'
 import {LMap, LTileLayer} from 'vue2-leaflet';
 
 export default {
@@ -103,6 +104,7 @@ export default {
         ocGeojson,
         sdohData,
         weightedSDOHData,
+        weighted_mhi_data,
         selectedMap: 'map1',
         colorScale: ["71ae46", "ebe12a", "ac2026"],
         foodValue: {
@@ -110,8 +112,8 @@ export default {
           metric: "Food Stamps"
         },
         ocValue: {
-          key: "mhi_avg",
-          metric: "Mental Health Index"
+          key: "w_mhi_avg",
+          metric: "Weighted Mental Health Index Average"
         },
         econValue:{
           key: "ACS_PCT_UNEMPLOY_ZC",
