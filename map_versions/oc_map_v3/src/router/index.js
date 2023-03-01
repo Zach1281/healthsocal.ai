ca56efd58b624c552de6d2d27cb6da987eb03f0e
tiffanyttran <tttran10@uci.edu>
2023-03-01 12:57:16 -0800

    fixed errors

--- map_versions/oc_map_v3/src/router/index.js
+++ map_versions/oc_map_v3/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import Home from '@/components/HomePage.vue'
 import Map from '@/components/MapPage.vue'
 
@@ -17,19 +16,6 @@ const routes = [
     name: 'Map',
     component: Map
   }
-  /*{
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" '../views/AboutView.vue')
-  }*/
 ]
 
 const router = new VueRouter({
